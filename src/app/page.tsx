c68121b5f8c3eeea4f4bec53c48c02c7e8a9c259
Indujan Y <indujanyogeswaran@gmail.com>
2025-07-14 06:12:39 +0000

    the page again and again coming to login even after successful authentic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,13 +14,14 @@ import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { app } from '@/lib/firebase';
 
 function setCookie(name: string, value: string, days: number) {
-  let expires = '';
+  let expires = "";
   if (days) {
     const date = new Date();
     date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    expires = '; expires=' + date.toUTCString();
+    expires = "; expires=" + date.toUTCString();
   }
-  document.cookie = name + '=' + (value || '')  + expires + '; path=/';
+  // Ensure the cookie is accessible by the entire site
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
 
 
